e10be807ae44346f535d64f8ea04a4b1fc4ea704
sadat <shameemsadat00@gmail.com>
2025-02-01 14:31:50 +0600

    working registration api

--- backend/controllers/authControllers.js
+++ backend/controllers/authControllers.js
@@ -14,8 +14,7 @@ export const register = async (req,res)=>{
 
     try {
 
-        // const existingUser = await userModel.findOne({email}); ///undo when database is ready
-        const existingUser = false; /// remove this line when database is ready
+        const existingUser = await userModel.findOne({email}); 
 
 
         if(existingUser){
@@ -28,14 +27,8 @@ export const register = async (req,res)=>{
         const hashedPass = await bcrypt.hash(password,10);  //10, encryption level, bigger number stronger but takes time, 10 is default ok
 
 
-        /* -----------------UNDO COMMENTS WHEN DATABASE IS READY
-
         const user = new userModel({name,email,password:hashedPass});
         await user.save(); //this object is saved in the database
-
-        --------------------------------------------------------------------------*/ 
-        const user = { _id: "12345", name, email, password: hashedPass }; //DELETE WHEN DATABASE IS READY
-
 
         const token = jwt.sign({id: user._id,}, process.env.JWT_SECRET, {expiresIn:'1d'});
 
