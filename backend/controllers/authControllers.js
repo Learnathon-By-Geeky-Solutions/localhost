41e35113616dc6589a877d3491151f42a7ed76c7
SamihaShormi <noshinsamihashormi@gmail.com>
2025-02-02 04:04:31 +0600

    renamed reg to signup

--- backend/controllers/authControllers.js
+++ backend/controllers/authControllers.js
@@ -2,7 +2,7 @@ import bcrypt from 'bcryptjs';
 import jwt from 'jsonwebtoken';
 import userModel from '../models/userModel.js';
 
-export const register = async (req,res)=>{
+export const signup = async (req,res)=>{
     const {name,email,password} = req.body;
 
     if(!name || !email || !password){
