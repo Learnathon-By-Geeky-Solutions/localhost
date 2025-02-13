357faaf6cbc1166388186a57f4e40c35f5373f10
sadat <shameemsadat00@gmail.com>
2025-02-13 12:22:07 +0600

    Merged backend from sadat/auth into dev

--- backend/server.js
+++ backend/server.js
@@ -1,32 +1,33 @@
-import express from "express"
-import cors from "cors"
-import "dotenv/config"; // this works directly instead of iporting dotenv, then dotenv.config()
+
+import express from "express";
+import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
-import authRouter from "./routes/auth.routes.js";
-import connectDB from "./config/mongodb.js";
+import cors from "cors";
+
+import connectDB  from "./config/mongodb.js";
+
+import authRoutes from "./routes/auth.routes.js";
 
 
 const app = express();
-connectDB();
+dotenv.config();
 
 
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors({
-    credentials:true,
-}));   
+app.use(
+  cors({
+    origin: `${process.env.BASE_URL}:${process.env.CLIENT_PORT}`,
+    credentials: true,
+  })
+);
+
+app.use("/api/auth", authRoutes);
 
 
-app.get('/', (req,res)=>{
-    res.send("root page api response");
-})
-app.use('/api/auth',authRouter);
 
-
-
-const port = process.env.PORT || 8000;
-const DOMAIN = process.env.SERVER_DOMAIN;
-app.listen(port, ()=>{
-    console.log(`server is running on port ${DOMAIN}:${port}`);
+const port = process.env.SERVER_PORT;
+app.listen(port, () => {
+  console.log(`server is running on ${process.env.BASE_URL}:${port}`);
+  connectDB();
 });
-
