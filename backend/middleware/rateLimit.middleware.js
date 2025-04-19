161c8a37becd75405c507a4aa426d74f2aa45c66
sadat <shameemsadat00@gmail.com>
2025-04-19 15:18:57 +0600

    increased request limit in rate limiter

--- backend/middleware/rateLimit.middleware.js
+++ backend/middleware/rateLimit.middleware.js
@@ -2,7 +2,7 @@ import rateLimit from "express-rate-limit";
 
 export const rateLimiter = rateLimit({
   windowMs: 10 * 60 * 1000, // 10 minutes
-  max: 5, // Max 5 requests per IP
+  max: 1000, // Max 5 requests per IP
   message: "Too many requests, please try again later.",
   standardHeaders: true,
   legacyHeaders: false,
