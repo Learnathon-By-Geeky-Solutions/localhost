2d28decd93780307399607c36da01c77609fe3b0
SamihaShormi <noshinsamihashormi@gmail.com>
2025-04-20 14:42:17 +0600

    taskCalendar as planner component with one issue

--- frontend/src/pages/Planner.jsx
+++ frontend/src/pages/Planner.jsx
@@ -1,13 +1,14 @@
-import React from 'react'
-import styles from './planner.module.css'
-
+import React from "react";
+import styles from "./planner.module.css";
+import TaskCalendar from "../components/TaskCalendar";
 
 const Planner = () => {
   return (
-    <div>
-      <h1>PLANNER</h1>
+    <div className={styles.container}>
+      <h2 className={styles.plannerHeader}>Planner Calendar</h2>
+      <TaskCalendar />
     </div>
-  )
-}
+  );
+};
 
-export default Planner
+export default Planner;
