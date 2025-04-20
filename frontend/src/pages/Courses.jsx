b2572b187380fc29178db676a5a0b7b6eb41e393
sadat <shameemsadat00@gmail.com>
2025-04-21 02:28:54 +0600

    another test fix

--- frontend/src/pages/Courses.jsx
+++ frontend/src/pages/Courses.jsx
@@ -58,7 +58,7 @@ const Courses = () => {
 
   return (
     <div className={styles.container}>
-      <div className={styles.card} onClick={() => setShowForm(true)}>
+      <button className={styles.card} onClick={() => setShowForm(true)}>
         <div 
           className={styles.icon}
         >
@@ -66,7 +66,7 @@ const Courses = () => {
             <path fillRule="evenodd" d="M12 2.25c-5.385 0-9.75 4.365-9.75 9.75s4.365 9.75 9.75 9.75 9.75-4.365 9.75-9.75S17.385 2.25 12 2.25ZM12.75 9a.75.75 0 0 0-1.5 0v2.25H9a.75.75 0 0 0 0 1.5h2.25V15a.75.75 0 0 0 1.5 0v-2.25H15a.75.75 0 0 0 0-1.5h-2.25V9Z" clipRule="evenodd" />
           </svg>
         </div>
-      </div>
+      </button>
 
       {courses.map((course) => (
         <button
